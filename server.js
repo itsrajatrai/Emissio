853555c35b5cae2f40f159b4b21c900c8c742238
therajatrai <therajatraiofficial@gmail.com>
2022-07-14 14:30:00 +0530

    Update

--- server.js
+++ server.js
@@ -129,8 +129,9 @@ app.get('/store', (req, res) => {
 
 var http = require('http')
  var port = process.env.PORT || 3000;
- http.createServer(function(req, res) {
-   res.writeHead(200, { 'Content-Type': 'text/plain' });
-   res.end('Hello World\n');
- }).listen(port);
-);
+    http.createServer(app).listen(port, function () {
+        console.log('Server listening on port ' + port);
+    }
+    );
+    
+
